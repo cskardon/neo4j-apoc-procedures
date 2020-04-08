c41789c00abab4d1b95dd9609c93ffc602c3ec31
Chris Skardon <chris@tournr.com>
2020-04-08 10:34:12 +0100

    Adds `apoc.bytes.*` functions
    RE: issue-784
    Adds:
     `apoc.bytes.fromLong`
     `apoc.bytes.toLong`
     `apoc.bytes.toHexString`
     `apoc.bytes.fromHexString`

--- src/main/java/apoc/bytes/Bytes.java
+++ src/main/java/apoc/bytes/Bytes.java
@@ -4,6 +4,7 @@ import apoc.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserFunction;
 
+import java.nio.ByteBuffer;
 import java.util.regex.Pattern;
 
 import static java.lang.Integer.parseInt;
@@ -12,6 +13,7 @@ import static java.lang.Math.min;
 public class Bytes {
 
     public static final Pattern HEXSTRING = Pattern.compile("^[0-9A-F]+$", Pattern.CASE_INSENSITIVE);
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
     @UserFunction("apoc.bytes.fromHexString")
     @Description("turns hex string into array of bytes, invalid values will result in null")
@@ -21,13 +23,54 @@ public class Bytes {
         if (!HEXSTRING.matcher(text).matches()) return null;
 
         int textLen = text.length();
-        int bytesCount = textLen / 2 + textLen % 2;
+        if(textLen % 2 == 1) {
+            textLen++;
+            text = String.format("0%s", text);
+        }
 
+        int bytesCount = textLen / 2 + textLen % 2;
         byte[] result = new byte[bytesCount];
+
         for (int pos = 0; pos < bytesCount; pos++) {
             int index = pos * 2;
             result[pos] = (byte) parseInt(text.substring(index, min(index + 2, textLen)), 16);
         }
         return result;
+    }
+
+    @UserFunction("apoc.bytes.toHexString")
+    @Description("Turns a byte array into a hex string. Invalid values will result in null.")
+    public String toHexString(@Name("bytes") byte[] bytes){
+        //Original code from: https://stackoverflow.com/questions/9655181/how-to-convert-a-byte-array-to-a-hex-string-in-java
+        if(bytes == null || bytes.length == 0)
+            return null;
+
+        char[] hexChars = new char[bytes.length * 2];
+        for (int i = 0; i < bytes.length; i++) {
+            int v = bytes[i] & 0xFF;
+            hexChars[i * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[i * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+
+        if(hexChars[0] == '0'){
+            return new String(hexChars, 1, hexChars.length - 1);
+        }
+
+        return new String(hexChars);
+    }
+
+    @UserFunction("apoc.bytes.fromLong")
+    @Description("Turns a long into an array of bytes.")
+    public byte[] fromLong(@Name("value") Long value){
+        ByteBuffer buffer = ByteBuffer.allocate(8);
+        buffer.putLong(value);
+        return buffer.array();
+    }
+
+    @UserFunction("apoc.bytes.toLong")
+    @Description("Turns a byte array into a long.")
+    public Long toLong(@Name("bytes") byte[] bytes) {
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        return buffer.getLong();
     }
 }
--- src/test/java/apoc/bytes/BytesTest.java
+++ src/test/java/apoc/bytes/BytesTest.java
@@ -5,7 +5,6 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.test.rule.DbmsRule;
@@ -35,15 +34,22 @@ public class BytesTest {
     @Test
     public void fromHexString() {
         byte[] expected = new byte[]{(byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE};
-        byte[] actual = new Bytes().fromHexString("CaFEBaBE");
+        byte[] actual = BYTES.fromHexString("CaFEBaBE");
         assertArrayEquals("expected CaFEBaBE byte array", expected, actual);
     }
 
     @Test
-    public void fromHexStringOddlLength() {
-        byte[] expected = new byte[]{(byte) 0xAB, (byte) 0x0C};
-        byte[] actual = new Bytes().fromHexString("ABC");
+    public void fromHexStringOddLength() {
+        byte[] expected = new byte[]{(byte) 0x0A, (byte) 0xBC};
+        byte[] actual = BYTES.fromHexString("ABC");
         assertArrayEquals("expected ABC byte array", expected, actual);
+    }
+
+    @Test
+    public void fromHexStringEvenLength() {
+        byte[] expected = new byte[]{(byte) 0xAB, (byte) 0xBA};
+        byte[] actual = BYTES.fromHexString("ABBA");
+        assertArrayEquals("expected ABBA byte array", expected, actual);
     }
 
     @Test
@@ -66,4 +72,109 @@ public class BytesTest {
         }
     }
 
+    @Test
+    public void toHexString(){
+        final byte[] input = new byte[]{(byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE};
+        final String expected = "CAFEBABE";
+        final String actual = BYTES.toHexString(input);
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void toHexStringOddLength(){
+        final byte[] input = new byte[]{(byte) 0x0A, (byte) 0xBC};
+        final String expected = "ABC";
+        final String actual = BYTES.toHexString(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void toHexStringNull() {
+        assertNull("On null argument", BYTES.toHexString(null));
+        assertNull("On empty argument", BYTES.toHexString(new byte[0]));
+    }
+
+    @Test
+    public void toHexStringUsingFunction() {
+        Map<String, Object> parameters = Collections.singletonMap("bytes", new byte[]{(byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE});
+        String query = "RETURN apoc.bytes.toHexString($bytes) AS value";
+        final String expected = "CAFEBABE";
+
+        try (Transaction tx = db.beginTx()) {
+            final ResourceIterator<String> actual = tx.execute(query, parameters).columnAs("value");
+            assertEquals("expected CAFEBABE byte array", expected, actual.next());
+            assertFalse(actual.hasNext());
+        }
+    }
+
+    @Test
+    public void fromLongMax() {
+        final byte[] expected = new byte[]{(byte) 0x7F, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
+        final byte[] actual = BYTES.fromLong(Long.MAX_VALUE);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void fromLongMin() {
+        final byte[] expected = new byte[]{(byte) 0x80, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00};
+        final byte[] actual = BYTES.fromLong(Long.MIN_VALUE);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void fromLongZero() {
+        final byte[] expected = new byte[]{(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00};
+        final byte[] actual = BYTES.fromLong(0L);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void fromLongUsingFunction() {
+        Map<String, Object> parameters = Collections.singletonMap("value", Long.MAX_VALUE);
+        String query = "RETURN  apoc.bytes.fromLong($value) AS value";
+        final byte[] expected = new byte[]{(byte) 0x7F, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
+
+        try (Transaction tx = db.beginTx()) {
+            final ResourceIterator<byte[]> actual = tx.execute(query, parameters).columnAs("value");
+            assertArrayEquals("Expected 7FFFFFFFFFFFFFFF byte array", expected, actual.next());
+            assertFalse(actual.hasNext());
+        }
+    }
+
+    @Test
+    public void toLongMax(){
+        final Long expected = Long.MAX_VALUE;
+        final byte[] bytes = new byte[]{(byte) 0x7F, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
+        final Long actual = BYTES.toLong(bytes);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void toLongMin(){
+        final Long expected = Long.MIN_VALUE;
+        final byte[] bytes = new byte[]{(byte) 0x80, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00};
+        final Long actual = BYTES.toLong(bytes);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void toLongZero(){
+        final Long expected = 0L;
+        final byte[] bytes = new byte[]{(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00};
+        final Long actual = BYTES.toLong(bytes);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void toLongUsingFunction() {
+        final Map<String, Object> parameters = Collections.singletonMap("value", new byte[]{(byte) 0x7F, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF});
+        final String query = "RETURN  apoc.bytes.toLong($value) AS value";
+        final Long expected = Long.MAX_VALUE;
+
+        try (Transaction tx = db.beginTx()) {
+            final ResourceIterator<Long> actual = tx.execute(query, parameters).columnAs("value");
+            assertEquals(expected, actual.next());
+            assertFalse(actual.hasNext());
+        }
+    }
 }
